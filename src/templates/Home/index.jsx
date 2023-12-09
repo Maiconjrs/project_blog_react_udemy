aa1d2aa08f3a1a087ef2e1da48d0d00d1840ff49
Maiconjrs <maiconjrs@hotmail.com>
2023-12-09 16:45:03 -0300

    chande class component from hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,80 +1,73 @@
 import "./styles.css";
 
-import { Component } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { loadPosts } from "../../utils/load-posts";
 import { Posts } from "../../components/Posts";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/TextInput";
 
-class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 20,
-    searchValue: "",
-  };
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState("");
 
-  async componentDidMount() {
-    await this.loadPosts();
-  }
+  const noMorePosts = page + postsPerPage >= allPosts.length ? true : false;
 
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
+  const filterdPosts = !!searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : posts;
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos,
-    });
-  };
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  },[]);
 
-  loadMorePosts = () => {
-    const { page, postsPerPage, allPosts, posts } = this.state;
+  useEffect(() => {
+    console.log('oi')
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
     posts.push(...nextPosts);
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-    this.setState({ searchValue: value });
+    setSearchValue(value);
   };
 
-  render() {
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length ? true : false;
+  return (
+    <section className="container">
+      <div className="search-container">
+        {!!searchValue && <h1>Search Value: {searchValue}</h1>}
 
-    const filterdPosts = !!searchValue
-      ? allPosts.filter((post) => {
-          return post.title.toLowerCase().includes(searchValue.toLowerCase());
-        })
-      : posts;
-    return (
-      <section className="container">
-        <div className="search-container">
-          {!!searchValue && <h1>Search Value: {searchValue}</h1>}
+        <Input value={searchValue} onChange={handleChange} />
+      </div>
 
-          <Input value={searchValue} onChange={this.handleChange} />
-        </div>
+      {filterdPosts.length > 0 && <Posts posts={filterdPosts} />}
+      {filterdPosts.length === 0 && (
+        <p>Não exitem posts relacionados ao termo: {searchValue} </p>
+      )}
 
-        {filterdPosts.length > 0 && <Posts posts={filterdPosts} />}
-        {filterdPosts.length === 0 && (
-          <p>Não exitem posts relacionados ao termo: {searchValue} </p>
+      <div className="button-container">
+        {!searchValue && (
+          <Button
+            text="Load more Posts"
+            onClick={loadMorePosts}
+            disabled={noMorePosts}
+          />
         )}
-
-        <div className="button-container">
-          {!searchValue && (
-            <Button
-              text="Load more Posts"
-              onClick={this.loadMorePosts}
-              disabled={noMorePosts}
-            />
-          )}
-        </div>
-      </section>
-    );
-  }
-}
-
+      </div>
+    </section>
+  );
+};
 export default Home;
