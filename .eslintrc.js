7446e92deb87199baa36d64ac8f9db2b7284641b
Maiconjrs <maiconjrs@hotmail.com>
2023-12-19 21:30:25 -0300

    finalização testes projeto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,5 +1,111 @@
-describe("<Home/>", () => {
-  it("is a dummy test", () => {
-    expect(1).toBe(1);
+import { rest } from 'msw';
+import { userEvent } from '@testing-library/user-event';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import Home from '.';
+import React from 'react';
+import { setupServer } from 'msw/node';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          body: 'body1',
+          url: 'img1',
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+          url: 'img2',
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: 'title3',
+          body: 'body3',
+          url: 'img3',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+describe('<Home/>', () => {
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+  it('should render search, posts and load more', async () => {
+    console.log('carregou');
+    render(<Home />);
+
+    const noMorePosts = screen.getByText('Não exitem posts relacionados ao termo:');
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInThedocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInThedocument();
+
+    screen.debug();
+  });
+
+  it('should seaRCH FOR POSTS', async () => {
+    render(<Home />);
+
+    const noMorePosts = screen.getByText('Não exitem posts relacionados ao termo:');
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInThedocument();
+
+    expect(screen.getByRole('heading', { name: 'Title1 1' })).toBeInThedocument();
+    expect(screen.getByRole('heading', { name: 'Title2 2' })).toBeInThedocument();
+    expect(screen.queryByRole('heading', { name: 'Title3 3' })).not.toBeInThedocument();
+
+    userEvent.type(search, 'title1');
+    expect(screen.getByRole('heading', { name: 'Title1 1' })).toBeInThedocument();
+    expect(screen.queryByRole('heading', { name: 'Title2 2' })).not.toBeInThedocument();
+    expect(screen.queryByRole('heading', { name: 'Title3 3' })).not.toBeInThedocument();
+
+    expect(screen.getByRole('heading', { name: 'Search value: title1' }));
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'Title1 1' })).toBeInThedocument();
+    expect(screen.getByRole('heading', { name: 'Title2 2' })).toBeInThedocument();
+
+    userEvent.type(search, 'blábla');
+    expect(screen.getByText('Não exitem posts relacionados ao termo:')).toBeInThedocument();
+  });
+
+  it('should load more posts', async () => {
+    render(<Home />);
+
+    const noMorePosts = screen.getByText('Não exitem posts relacionados ao termo:');
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    userEvent.click(button);
+
+    expect(screen.getByRole('heading', { name: 'Title3 3' })).toBeInThedocument();
+    expect(button).toBeDisabled();
   });
 });
